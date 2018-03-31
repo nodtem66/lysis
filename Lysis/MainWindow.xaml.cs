093744004a5460d6331622a4a9f2f07a7b6dcdb4
Jirawat I <nodtem66@gmail.com>
2018-03-31 14:41:49 +0700

    first commit

---
+++ Lysis/MainWindow.xaml.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using Microsoft.Win32;
+using MahApps.Metro.Controls;
+
+namespace Lysis
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : MetroWindow
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuFileOpen_Click(object sender, RoutedEventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            openFileDialog.Filter = "Image files|*.jpg;*.gif;*.png;*.bmp";
+            if (openFileDialog.ShowDialog() == true)
+            {
+                ImageBrush brush = new ImageBrush();
+                brush.ImageSource = new BitmapImage(new Uri(openFileDialog.FileName, UriKind.Relative));
+                brush.Stretch = Stretch.Uniform;
+                brush.Freeze();
+                canvas1.Background = brush;
+                statusText.Content = "Open " + openFileDialog.FileName;
+            }
+        }
+    }
+}
